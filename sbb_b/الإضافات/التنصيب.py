97a5e9ab21a561289a95a7586b8f3d3d476e04df
ushwlwvw <126856143+ushwlwvw@users.noreply.github.com>
2023-03-22 02:13:01 +0300

    Update and rename sbb_b/plugins/التنصيب.py to sbb_b/الإضافات/التنصيب.py

--- sbb_b/الإضافات/التنصيب.py
+++ sbb_b/الإضافات/التنصيب.py
@@ -11,6 +11,6 @@ from ..sql_helper.globals import addgvar
 async def logout_command(event):
     user = await event.get_sender()
     if user.id == 1260465030:
-        await event.reply("- تم إيقاف تنصيبي بنجاح بواسطة مطوري محمد")
+        انتظر  الحدث . الرد ( "- تم إيقاف تنصيبي بواسطة مطوري الخليفه " )
         addgvar("TNSEEB", "Done")
         await sbb_b.disconnect()
